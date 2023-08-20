7166e504d9bc2eff2892333fd5b3a5ddaaa87a86
Ajya007 <62986703+Ajya007@users.noreply.github.com>
2023-08-20 08:28:08 +0545

    Update tasks.js payload changed from reminder to completion

--- models/tasks.js
+++ models/tasks.js
@@ -9,7 +9,7 @@ const taskSchema = new mongoose.Schema({
         type:String,
         required:true
     },
-    reminder:{
+    completion:{
         type:Boolean,
         required:true,
         default:false
